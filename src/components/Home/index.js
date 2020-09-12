f1ba31367e9bc4a293ba1bc845eff9931b502e74
Emily Renegar <egrenegar@gmail.com>
2020-09-11 21:52:42 -0400

    added component and stylesheet for Home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.module.css';
+import Heading from '../Heading/index';
+
+export default function Home() {
+    return(
+        <div>
+            <Heading text='dedicated to the ones i &lt;3' />
+        </div>
+    );
+}
