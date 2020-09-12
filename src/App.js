024f5696cd5de9ccc85c9760d0a2696b4d75b473
Emily Renegar <egrenegar@gmail.com>
2020-09-11 21:56:10 -0400

    added path for Friends page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import Friends from './pages/Friends';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/Friends' component={Friends} />
       </div>
     </Router>
-
   );
 }
 
