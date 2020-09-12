ce11862af7d94b0eeef882fc0b5e72da770406b0
Emily Renegar <egrenegar@gmail.com>
2020-09-12 17:24:03 -0400

    moved 'enter site' link into Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './home.module.css';
+import styles from './home.module.css';
 import Heading from '../Heading/index';
 
 export default function Home() {
-    return(
-        <div>
-            <Heading text='dedicated to the ones i &lt;3' />
+    return (
+        <div className={styles.homeDiv}>
+            <Heading text='dedicated to the ones i &lt;3' className={styles.headingMain}/>
+            <p className={styles.subtitle}>y'all rule.</p>
+            <span className={styles.arrow}>&#10142;</span>
+            <div>
+                <a href="/Friends" className={styles.enter}>
+                <span><em>enter site</em></span>
+                </a>
+            </div>
         </div>
     );
 }
