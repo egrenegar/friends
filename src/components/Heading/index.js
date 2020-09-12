106eb13d5177a2a4597a030a2c73102b6b291ecf
Emily Renegar <egrenegar@gmail.com>
2020-09-12 17:20:27 -0400

    wrapped <h1> in a <div>

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -3,8 +3,10 @@ import styles from './heading.module.css';
 
 export default function Heading(props) {
     return(
-        <h1 className={styles.heading}>
-            {props.text}
-        </h1>
+        <div>
+            <h1 className={styles.heading}>
+                {props.text}
+            </h1>
+        </div>
     );
 }
