20648c8a7d88bb5398ec7260325ebbf8f8549f34
Emily Renegar <egrenegar@gmail.com>
2020-09-11 21:56:50 -0400

    imported Heading & FriendsGrid components

---
+++ src/pages/Friends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading from '../components/Heading/index';
+import FriendsGrid from '../components/FriendsGrid/index';
+
+export default function Friends() {
+    return (
+        <div>
+            <Heading text='my friends !!!!' />
+            <FriendsGrid />
+        </div>
+    );
+}
