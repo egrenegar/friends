f7a9b82b094a8331fc703d14b2520d0144c4c789
Emily Renegar <egrenegar@gmail.com>
2020-09-24 19:57:34 -0400

    added Bulma grid to Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Columns, Container } from 'react-bulma-components';
 import styles from './home.module.css';
 import Heading from '../Heading/index';
 
 export default function Home() {
     return (
-        <div className={styles.homeDiv}>
-            <Heading text='dedicated to the ones i &lt;3' className={styles.headingMain}/>
-            <p className={styles.subtitle}>y'all rule.</p>
-            <span className={styles.arrow}>&#10142;</span>
-            <div>
-                <a href="/Friends" className={styles.enter}>
-                <span><em>enter site</em></span>
+        <Container className={styles.homeDiv}>
+            <Columns className='is-centered'>
+                <Columns.Column className={styles.headingDiv}>
+                    <Heading text='dedicated to the ones i &lt;3'/>
+                    <p className={styles.subtitle}>y'all rule.</p>
+                </Columns.Column>
+            </Columns>
+            <Columns className='is-centered'>
+                <Columns.Column>
+                    <span className={styles.arrow}>&#10142;</span>
+                </Columns.Column>
+            </Columns>
+            <Columns className='is-centered'>
+                <a href='/Friends' className={styles.enter}>
+                    <span><em>enter site</em></span>
                 </a>
-            </div>
-        </div>
+            </Columns>
+        </Container>
     );
 }
