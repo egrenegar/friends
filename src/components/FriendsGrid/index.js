85b19860d29286b9cdb04388b999110c006cde05
Emily Renegar <egrenegar@gmail.com>
2020-09-24 19:54:06 -0400

    added Bulma grid to Friends page

--- src/components/FriendsGrid/index.js
+++ src/components/FriendsGrid/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import Heading from '../Heading/index';
+import { Columns, Container } from 'react-bulma-components';
 
 export default function FriendsGrid() {
     return (
-        <p>placeholder</p>
+        <Container>
+            <Columns className='is-centered'>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+            </Columns>
+            <Columns className='is-centered'>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+            </Columns>
+            <Columns className='is-centered'>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+                <Columns.Column size={3}>
+                    <p>test</p>
+                </Columns.Column>
+            </Columns>
+        </Container>
     );
 }
